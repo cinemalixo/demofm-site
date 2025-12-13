6e0ad9d642cc75c7ad6f397357573135d75d5223
cinemalixo <cinemalixo@gmail.com>
2025-12-12 22:14:32 -0500

    Update news.js

--- data/news.js
+++ data/news.js
@@ -1,1 +1,10 @@
-a
+[
+  {
+    "title": "New archive uploaded",
+    "link": "https://example.com"
+  },
+  {
+    "title": "Late summer broadcast announced",
+    "link": "https://example.com"
+  }
+]
